dc72c270f47dabd784cf69e976525860f169d5fd
seonghun <shanash511@gmail.com>
2019-06-14 13:41:05 +0900

    정상적인 시나리오 하에서만 돌아가게 세팅

--- server/controllers/FileController.js
+++ server/controllers/FileController.js
@@ -4,84 +4,15 @@ import fs from 'fs';
 
 var FileController = class FileController extends BaseController {
   async add(req, res, next) {
-    const isJsonString = (str) => {
-      try {
-        JSON.parse(str);
-      } catch(e) {
-        return false;
-      }
-      return true;
-    }
-    const convertDataForClient = (data) => {
-      var result = new Object();
-      Object.keys(data).forEach(function(key) {
-        switch(key) {
-          case 'Common':
-            var commonData = data[key]; 
-            var jCommon = new Object();
-            Object.keys(commonData).forEach(function(index) {
-              if (index != 0) {
-                var jarr = new Array();
-                var arrValue = commonData[index]['B'].toString().split('\r\n');
-                arrValue.forEach(function (item, index, array) {
-                  jarr.push(item);
-                });
-                jCommon[commonData[index]['A']] = jarr;
-              }
-            });
-            result['Common'] = jCommon;
-            break;
-          case 'List':
-            var listData = data[key];
-            var jList = new Array();
-            Object.keys(listData).forEach(function(index) {
-              if (index != 0) {
-                var jobj = new Object();
-                Object.keys(listData[index]).forEach(function(innerKey) {
-                  jobj[listData[0][innerKey]] = listData[index][innerKey];
-                });
-                jList.push(jobj);
-              }
-            });
-            result['List'] = jList;
-            break;
-          case 'Dictionary':
-            var dicData = data[key];
-            var jDic = new Object();
-            Object.keys(dicData).forEach(function(index) {
-              if (index != 0) {
-                var jobj = new Object();
-                Object.keys(dicData[index]).forEach(function(innerKey) {
-                  if (innerKey != 'A') {
-                    jobj[dicData[0][innerKey]] = dicData[index][innerKey];
-                  }
-                });
-                jDic[dicData[index]['A']] = jobj;
-              }
-            });
-            result['Dictionary'] = jDic;
-            break;
-        }
-      });
-
-      return result;
-    }
-
-    super.add();
-    let path = req.file.destination + req.file.filename;
-    let readStream = fs.createReadStream(path);
-    
-    let chunks = [];
-
-    readStream.on('readable', function() {
+    const onReadStreamReadable = () => {
       let chunk = readStream.read();
       if (null != chunk)
       {
         chunks.push(chunk);
       }
-    });
+    }
 
-    readStream.on('end', function() {
+    const onReadStreamEnd = () => {
       let dir = './data';
   
       if (false == fs.existsSync(dir)) {
@@ -107,15 +38,102 @@ var FileController = class FileController extends BaseController {
       let writeStream = fs.createWriteStream('./data/' + req.body.name + '.json');
       writeStream.write(strJson);
       writeStream.end();
-    });
+
+    }
+
+    const isJsonString = (str) => {
+      try {
+        JSON.parse(str);
+      } catch(e) {
+        return false;
+      }
+      return true;
+    }
+    const convertValue = (value) => {
+      var strValue = value.toString();
+      if ( true == strValue.includes('\r\n') ) {
+        var jarr = new Array();
+        var arrValue = strValue.split('\r\n');
+        arrValue.forEach(function (item, index, array) {
+          jarr.push(item);
+        });
+
+        return jarr;
+      }
+
+      return value;
+    }
+
+    const convertDataForClient = (data) => {
+      var result = new Object();
+      Object.keys(data).forEach(function(key) {
+        switch(key) {
+          case 'Common':
+            var commonData = data[key]; 
+            var jCommon = new Object();
+            Object.keys(commonData).forEach(function(index) {
+              if (index != 0) {
+               jCommon[commonData[index]['A']] = convertValue(commonData[index]['B']);
+              }
+            });
+            result['Common'] = jCommon;
+            break;
+          case 'List':
+            var listData = data[key];
+            var jList = new Array();
+            Object.keys(listData).forEach(function(index) {
+              if (index != 0) {
+                var jobj = new Object();
+                Object.keys(listData[index]).forEach(function(innerKey) {
+                  jobj[listData[0][innerKey]] = convertValue(listData[index][innerKey]);
+                });
+                jList.push(jobj);
+              }
+            });
+            result['List'] = jList;
+            break;
+          case 'Dictionary':
+            var dicData = data[key];
+            var jDic = new Object();
+            Object.keys(dicData).forEach(function(index) {
+              if (index != 0) {
+                var jobj = new Object();
+                Object.keys(dicData[index]).forEach(function(innerKey) {
+                  if (innerKey != 'A') {
+                    jobj[dicData[0][innerKey]] = convertValue(dicData[index][innerKey]);
+                  }
+                });
+                jDic[dicData[index]['A']] = jobj;
+              }
+            });
+            result['Dictionary'] = jDic;
+            break;
+        }
+      });
+
+      return result;
+    }
+
+    super.add();
+    let path = req.file.destination + req.file.filename;
+    let readStream = fs.createReadStream(path);
+    
+    let chunks = [];
+
+    readStream.on('readable', onReadStreamReadable);
+    readStream.on('end', onReadStreamEnd);
   }
 
   async get(req, res, next) {
     super.get();
     const id = req.params.id;
+    const path = './data/' + id;
+    if (false == fs.existsSync(path))
+    {
+      return res.status(404);
+    }
 
-    const file = './data/' + id;
-    res.download(file);
+    res.download(path);
   }
 }
 
